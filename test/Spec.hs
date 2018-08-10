42854add986a111ce33e026aa330a6aedbf0a718
Emile Raffoul <eraffoul@icm-consulting.com.au>
2018-08-10 23:18:37 +1000

    add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,3 +8,9 @@ main = hspec $ do
   describe "Closest String Test" $ do
     it "basic happy case" $ do
       closest ["foo bar", "bar foo", "foos bards", "foobar"] `shouldBe` (("foo bar", "foobar") :: ([Char], [Char]))
+    it "empty collection" $ do
+      evaluate (closest []) `shouldThrow` anyException
+    it "single element collection" $ do
+      evaluate (closest []) `shouldThrow` anyException
+    it "2 element collection" $ do
+      closest ["this is a test", "of two"] `shouldBe` (("this is a test", "of two") :: ([Char], [Char]))
